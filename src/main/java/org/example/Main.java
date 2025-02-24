a037188b2acaa0cd6a5fe5a5a953c0093a518f1a
JuanesCapurroU <juan.capurro@uniminuto.edu.co>
2025-02-24 18:06:22 -0500

    Instrucciones y Subida Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,15 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
+        //Trabajo de investigacion de como funcionaba en Switch Case en las versiones de Java 7 y anteriores
+        // Y como funcionan hoy en dia.
+
+        //La explicacion y codigo se encuentran en las ramas:
+        // - feature/SwitchAntiguo
+        // - feature/SwitchNuevo
+
+        //Juan Esteban Capurro Buitrago
+        // ID: 833959
 
     }
 }
