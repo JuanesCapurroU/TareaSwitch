a9df625bd99521595251e645c8d41e34da0090a2
JuanesCapurroU <juan.capurro@uniminuto.edu.co>
2025-02-24 18:02:25 -0500

    SwitchCase Nuevo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,32 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
+        //Apartir de la version de Java 12 en adelante hubieronn cambios y mejoras en el  switch
+        // Se remplaza los dos puntos ":" a flechas ->, tambien estas flechas evitan colocar
+        // Los Switch con break en cada sentencia
+        // Tambien se pueden asignar valores a una variable sin necesidad de crearla antes
+
+        int dia = 3;
+
+        String nombreDia = switch (dia) {
+            case 1 -> "Lunes";
+            case 2 -> "Martes";
+            case 3 -> "Miércoles";
+            default -> "Día inválido";
+        };
+
+        System.out.println("Hoy es " + nombreDia);
+
+        //Tambien permite multiples casos en una sola linea, ejemplo:
+
+        char letra = 'a';
+
+        String tipo = switch (letra) {
+            case 'a', 'e', 'i', 'o', 'u' -> "Vocal";
+            default -> "Consonante";
+        };
+
+        System.out.println("La letra '" + letra + "' es una " + tipo);
 
     }
 }
